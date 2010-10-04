4bbac847e240349575f9a3400c2557a894e3a0f0
andy.malakov <andy.malakov@792c5d81-bf30-d53a-9cc9-95ae04a5e27c>
2010-10-04 05:02:44 +0000

    first beta

---
+++ MayaCalendar/src/org/sf/maya/calendar/model/MayaTimeUnit.java
@@ -0,0 +1,21 @@
+package org.sf.maya.calendar.model;
+
+public enum MayaTimeUnit {
+	
+	Baktun(144000),
+	Katun (7200),
+	Tun   (360),
+	Winal (20),
+	Kin   (1);
+	
+
+	public final int numberOfDays;
+	private MayaTimeUnit (int numberOfDays) {
+		this.numberOfDays = numberOfDays;
+	}
+	
+	public int toDays (int units) {
+		return numberOfDays * units;
+	}
+
+}
